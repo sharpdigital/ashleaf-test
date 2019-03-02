98ea3fb1a557f484ec17ea69c5e0df05f08dfcd1
Janos Csikos <janos@csikos.co.uk>
2019-03-02 19:43:16 +0000

    updating the functions region / location to europe

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,32 +3,36 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-exports.addReview = functions.https.onRequest((req, res) => {
-    const {name, location, review} = req.body;
+exports.addReview = functions
+    .region('europe-west1')
+    .https
+    .onRequest((req, res) => {
+        const {name, location, review} = req.body;
 
-    if (!name || !location || !review) {
-        return res.json({
-            error: true,
-            message: 'All fields are required!',
-        });
-    }
+        if (!name || !location || !review) {
+            return res.json({
+                error: true,
+                message: 'All fields are required!',
+            });
+        }
 
-    return admin.firestore()
-        .collection('reviews')
-        .add({
-            original: {
-                displayName: `${name}, ${location}`,
-                review,
-                isApproved: false,
-                createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-            },
-        })
-        .then((writeResult) => res.json({
-            error: false,
-            result: `Message with ID: ${writeResult.id} added.`,
-        }))
-        .catch(() => res.json({
-            error: true,
-            message: 'Something went wrong, please try again!',
-        }));
-});
+        return admin
+            .firestore()
+            .collection('reviews')
+            .add({
+                original: {
+                    displayName: `${name}, ${location}`,
+                    review,
+                    isApproved: false,
+                    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+                },
+            })
+            .then((writeResult) => res.json({
+                error: false,
+                result: `Message with ID: ${writeResult.id} added.`,
+            }))
+            .catch(() => res.json({
+                error: true,
+                message: 'Something went wrong, please try again!',
+            }));
+    });

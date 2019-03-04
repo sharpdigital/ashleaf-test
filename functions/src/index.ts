64ca241dc8c9f1f07aaa2d144251638468cc44f9
Janos Csikos <janos@csikos.co.uk>
2019-03-04 20:50:02 +0000

    adding cors handler for functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,10 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as cors from 'cors';
+
+console.log('ofcors', cors);
+
+const corsHandler = cors({origin: true});
 
 admin.initializeApp();
 
@@ -7,32 +12,34 @@ exports.addReview = functions
     .region('europe-west1')
     .https
     .onRequest((req, res) => {
-        const {name, location, review} = req.body;
+        corsHandler(req, res, () => {
+            const {name, location, review} = req.body;
 
-        if (!name || !location || !review) {
-            return res.json({
-                error: true,
-                message: 'All fields are required!',
-            });
-        }
+            if (!name || !location || !review) {
+                return res.json({
+                    error: true,
+                    message: 'All fields are required!',
+                });
+            }
 
-        return admin
-            .firestore()
-            .collection('reviews')
-            .add({
-                original: {
-                    displayName: `${name}, ${location}`,
-                    review,
-                    isApproved: false,
-                    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-                },
-            })
-            .then((writeResult) => res.json({
-                error: false,
-                result: `Message with ID: ${writeResult.id} added.`,
-            }))
-            .catch(() => res.json({
-                error: true,
-                message: 'Something went wrong, please try again!',
-            }));
+            return admin
+                .firestore()
+                .collection('reviews')
+                .add({
+                    original: {
+                        displayName: `${name}, ${location}`,
+                        review,
+                        isApproved: false,
+                        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+                    },
+                })
+                .then((writeResult) => res.json({
+                    error: false,
+                    result: `Message with ID: ${writeResult.id} added.`,
+                }))
+                .catch(() => res.json({
+                    error: true,
+                    message: 'Something went wrong, please try again!',
+                }));
+        });
     });

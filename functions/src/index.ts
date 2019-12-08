43099a8971a2e341a344d64b836c78cd56327876
János Csikós <csikosjanos@gmail.com>
2019-12-08 09:52:54 +0000

    Updates and admin added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,12 +3,12 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as rp from 'request-promise';
 
-const corsHandler = cors({origin: true});
+const corsHandler = cors({ origin: true });
 
 admin.initializeApp();
 
 function getMessage(error: boolean, message: string) {
-    return {error, message};
+    return { error, message };
 }
 
 function respondWithError(res: functions.Response, message: string) {
@@ -20,14 +20,14 @@ function respondWithSuccess(res: functions.Response, message: string) {
 }
 
 function saveReview(req: functions.Request, res: functions.Response) {
-    const {name, location, review} = req.body;
+    const { name, location, review } = req.body;
 
     admin
         .firestore()
         .collection('reviews')
         .add({
             original: {
-                displayName: `${name}, ${location}`,
+                displayName: `${ name }, ${ location }`,
                 review,
                 isApproved: false,
                 createdAt: admin.firestore.Timestamp.fromDate(new Date()),
@@ -35,7 +35,7 @@ function saveReview(req: functions.Request, res: functions.Response) {
         })
         .then((writeResult) => respondWithSuccess(
             res,
-            `The review (${writeResult.id}) added successfully.`,
+            `The review (${ writeResult.id }) added successfully.`,
         ))
         .catch(() => respondWithError(
             res,
@@ -58,7 +58,7 @@ function checkRecaptcha(req: functions.Request) {
 }
 
 function isValidRequest(req: functions.Request): boolean {
-    const {name, location, review} = req.body;
+    const { name, location, review } = req.body;
 
     return req.query.response && name && location && review;
 }
@@ -74,12 +74,11 @@ exports.addReview = functions
                         console.log('recaptcha result', result);
                         if (result.success) {
                             saveReview(req, res);
-                        }
-                        else {
-                            respondWithError(res, `Recaptcha verification failed. Are you a robot? ${result}`);
+                        } else {
+                            respondWithError(res, `Recaptcha verification failed. Are you a robot? ${ result }`);
                         }
                     })
-                    .catch(reason => respondWithError(res, `All fields are required! ${reason}`));
+                    .catch(reason => respondWithError(res, `All fields are required! ${ reason }`));
             } else {
                 respondWithError(res, 'All fields are required!');
             }
@@ -88,7 +87,7 @@ exports.addReview = functions
 
 
 function saveReservation(req: functions.Request, res: functions.Response) {
-    const {day, subscriberId} = req.body;
+    const { day, subscriberId } = req.body;
 
     admin
         .firestore()
@@ -99,16 +98,55 @@ function saveReservation(req: functions.Request, res: functions.Response) {
         })
         .then(() => respondWithSuccess(
             res,
-            `You have successfully reserved the leaf collected on day ${day}`,
+            `You have successfully reserved the leaf collected on day ${ day }`,
         ))
         .catch(() => respondWithError(
             res,
-            `Unfortunatelly the leaf collected on day ${day} is no longer available for reservation. Please select another available leaf.`,
+            `Unfortunately the leaf collected on day ${ day } is no longer available for reservation. Please select another available leaf.`,
+        ));
+}
+
+function removeReservation(req: functions.Request, res: functions.Response) {
+    const { day } = req.body;
+
+    const collection = admin
+        .firestore()
+        .collection('reservations');
+
+    collection
+        .where('day', '==', day)
+        .get()
+        .then(snapshot => {
+            if (snapshot.empty) {
+                respondWithError(res, `The leaf collected on day ${ day } is already released.`);
+            }
+
+            snapshot.forEach(doc => {
+                collection.doc(doc.id).delete().then(
+                    () => {
+                        respondWithSuccess(
+                            res,
+                            `You have successfully released the leaf collected on day ${ day }`,
+                        );
+                    },
+                    (rejectReason) => {
+                        respondWithError(
+                            res,
+                            `Unsuccessful delete: ${rejectReason}`,
+                        )
+                    },
+                );
+            });
+
+        })
+        .catch(() => respondWithError(
+            res,
+            `Unfortunately the leaf collected on day ${ day } is still reservation. Please try again.`,
         ));
 }
 
 function isValidReservationRequest(req: functions.Request): boolean {
-    const {day, subscriberId} = req.body;
+    const { day, subscriberId } = req.body;
 
     return day && subscriberId;
 }
@@ -120,6 +158,25 @@ exports.reserveLeaf = functions
         corsHandler(req, res, () => {
             if (isValidReservationRequest(req)) {
                 saveReservation(req, res);
+            } else {
+                respondWithError(res, 'All fields are required!');
+            }
+        });
+    });
+
+function isValidReleaseRequest(req: functions.Request): boolean {
+    const { day } = req.body;
+
+    return !!day;
+}
+
+exports.releaseLeaf = functions
+    .region('europe-west1')
+    .https
+    .onRequest((req, res) => {
+        corsHandler(req, res, () => {
+            if (isValidReleaseRequest(req)) {
+                removeReservation(req, res);
             } else {
                 respondWithError(res, 'All fields are required!');
             }
